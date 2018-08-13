7b42c122054e68bee665fd96d2a6769e2c370b76
sapphire1996 <adepelumi1996@gmail.com>
2018-08-13 16:21:33 +0100

    Basic code player

---
+++ index.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    console.log('jS working');
+
+    function updateOutput() {
+
+        $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+
+        document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+
+    }
+
+    $(".toggleButton").hover(function () {
+        $(this).addClass("highlightedButton");
+    }, function () {
+        $(this).removeClass("highlightedButton");
+    });
+    $(".toggleButton").click(function () {
+        $(this).toggleClass("active");
+        $(this).removeClass("highlightedButton");
+
+        var panelId = $(this).attr("id") + "Panel";
+        $("#" + panelId).toggleClass("hidden");
+
+        var numberOfActivePanel = 4 - $('.hidden').length;
+        $(".panel").width(($(window).width() / numberOfActivePanel) - 10);
+    })
+    $(".panel").height(($(window).height() - $("#header").height() - 15));
+    $(".panel").width(($(window).width() / 2) - 10);
+
+    updateOutput();
+
+    $("textarea").on('change keyup paste', function () {
+
+        updateOutput();
+    });
+});
